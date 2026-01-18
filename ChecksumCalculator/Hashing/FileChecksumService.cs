0938a4c4b9ba9b9cd9db738cec3880ce0268298f
MarioTonchev <mariotonchev500@gmail.com>
2026-01-18 21:19:07 +0200

    adding service for getting a file checksum

---
+++ ChecksumCalculator/Hashing/FileChecksumService.cs
@@ -0,0 +1,20 @@
+﻿using ChecksumCalculator.Model;
+
+namespace ChecksumCalculator.Hashing
+{
+	public class FileChecksumService
+	{
+		public static ChecksumResult Compute(string path, IChecksumCalculator calculator)
+		{
+			using FileStream fs = File.OpenRead(path);
+			string checksum = calculator.Calculate(fs);
+
+			return new ChecksumResult
+			{
+				Path = path,
+				Checksum = checksum,
+				Size = fs.Length
+			};
+		}
+	}
+}
