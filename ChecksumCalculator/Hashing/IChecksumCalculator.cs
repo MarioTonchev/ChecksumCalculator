17145cd2c175ffbb4dcf7203e9718f72017bae5f
MarioTonchev <mariotonchev500@gmail.com>
2026-01-18 21:18:53 +0200

    adding checksumcalculator interface and implementation

---
+++ ChecksumCalculator/Hashing/IChecksumCalculator.cs
@@ -0,0 +1,7 @@
+﻿namespace ChecksumCalculator.Hashing
+{
+	public interface IChecksumCalculator
+	{
+		public string Calculate(Stream stream);
+	}
+}
---
+++ ChecksumCalculator/Hashing/Sha256ChecksumCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+
+namespace ChecksumCalculator.Hashing
+{
+	public class Sha256ChecksumCalculator : IChecksumCalculator
+	{
+		public string Calculate(Stream stream)
+		{
+			using SHA256 sha256 = SHA256.Create();
+			byte[] hash = sha256.ComputeHash(stream);
+			return Convert.ToHexString(hash).ToLowerInvariant();
+		}
+	}
+}
